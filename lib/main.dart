2ac0d956d006203cdb73d0f442b760c801378230
Alexander Pashinin <pashinin@Alexanders-MacBook-Air.local>
2022-12-11 20:38:29 +0500

    Added feature to loading rockets from API

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,40 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:space_x/app/riverpod/rocket.dart';
+import 'app/screen/rocket_screen.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends ConsumerWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      minTextAdapt: false,
+      designSize: const Size(360, 800),
+      builder: (context, child) => const CupertinoApp(
+        home: InitialScreen(),
+      ),
+    );
+  }
+}
+
+class InitialScreen extends ConsumerWidget {
+  const InitialScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final rockets = ref.watch(rocketsProvider);
 
-    return ScreenUtilInit(
-      minTextAdapt: false,
-      designSize: const Size(360, 800),
-      builder: (context, child) => CupertinoApp(
-        home: CupertinoPageScaffold(
-          child: rockets.when(
-              error: (error, _) => Center(child: Text(error.toString())),
-              loading: () => const Center(child: CupertinoActivityIndicator()),
-              data: (rockets) => Text('123')),
+    return CupertinoPageScaffold(
+      child: rockets.when(
+        error: (error, _) => Center(child: Text(error.toString())),
+        loading: () => const Center(child: CupertinoActivityIndicator()),
+        data: (rockets) => PageView(
+          children: rockets.map((rocket) => RocketScreen(rocket)).toList(),
         ),
       ),
     );
