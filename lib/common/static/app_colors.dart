e5f60fe6cb00cfc39a93a32af199531ff1e267ea
Alexander Pashinin <pashinin@Alexanders-MacBook-Air.local>
2022-12-11 20:33:44 +0500

    Added new colors

--- lib/common/static/app_colors.dart
+++ lib/common/static/app_colors.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/cupertino.dart';
 
 class AppColors {
-  static const titleColor = Color.fromRGBO(202, 202, 202, 1);
-  static const unitColor = Color.fromRGBO(142, 142, 143, 1);
+  static const title = Color.fromRGBO(202, 202, 202, 1);
+  static const unit = Color.fromRGBO(142, 142, 143, 1);
   static const launchesButton = Color.fromRGBO(33, 33, 33, 1);
+  static const unitItem = Color.fromRGBO(33, 33, 33, 1);
 }
