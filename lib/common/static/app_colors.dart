51e6b4e27be34d8fe257db6b4ad9677b6b04de07
Alexander Pashinin <pashinin@Alexanders-MacBook-Air.local>
2022-12-11 18:29:52 +0500

    Added custom colors

---
+++ lib/common/static/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static const titleColor = Color.fromRGBO(202, 202, 202, 1);
+  static const unitColor = Color.fromRGBO(142, 142, 143, 1);
+  static const launchesButton = Color.fromRGBO(33, 33, 33, 1);
+}
