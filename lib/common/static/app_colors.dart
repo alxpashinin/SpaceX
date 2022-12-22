f57de2f9c49bb5d935a2824356402ee7c31a1f8a
Alexander Pashinin <pashinin@Alexanders-MacBook-Air.local>
2022-12-22 19:38:33 +0500

    Added new colors

--- lib/common/static/app_colors.dart
+++ lib/common/static/app_colors.dart
@@ -5,4 +5,5 @@ class AppColors {
   static const unit = Color.fromRGBO(142, 142, 143, 1);
   static const launchesButton = Color.fromRGBO(33, 33, 33, 1);
   static const unitItem = Color.fromRGBO(33, 33, 33, 1);
+  static const settingsScreenBackground = Color.fromRGBO(18, 18, 18, 1);
 }
